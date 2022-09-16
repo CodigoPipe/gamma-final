13005958971e33b916bdef5ed7c4db855a015a3a
felipe <pipega2000@hotmail.com>
2022-09-16 13:34:50 -0500

    gamma for repo

---
+++ src/test/java/com/posada/santiago/gamapostsandcomments/GamaPostsAndCommentsApplicationTests.java
@@ -0,0 +1,9 @@
+package com.posada.santiago.gamapostsandcomments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class GamaPostsAndCommentsApplicationTests {
+
+}
